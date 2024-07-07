c3455e179faeb2a9e806349bef3cf75f09cb030b
Tristan <simontristan7766@gmail.com>
2024-07-07 18:18:11 +0200

    Added Uppercase PW

--- OnlyUppercasePasswords.ts
+++ OnlyUppercasePasswords.ts
@@ -1,7 +1,7 @@
 let Password = ""
 
 let lenght = 10;
-let chars = "abcdefghijklmnopqrstuvwxyz"
+let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 for (let i = 1; i <= lenght; i++) {
     let randomnum = Math.floor(Math.random() * chars.length) 
