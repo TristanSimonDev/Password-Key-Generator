185b959ee29fa2ba4593afb6c4ab447413c56064
Tristan <simontristan7766@gmail.com>
2024-07-07 17:56:24 +0200

    Lowercase

---
+++ OnlyLowercasePassword.ts
@@ -0,0 +1,13 @@
+
+
+let Password = ""
+
+let lenght = 10;
+let chars = "abcdefghijklmnopqrstuvwxyz"
+
+for (let i = 1; i <= lenght; i++) {
+    let randomnum = Math.floor(Math.random() * chars.length) 
+    Password = Password.concat(chars[randomnum])
+}
+
+console.log(Password)
