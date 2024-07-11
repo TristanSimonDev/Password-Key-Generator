50c6474ff4f3f8240468c167c76b1db91b1329ee
Tristan Simon <simontristan7766@gmail.com>
2024-07-11 07:27:16 -0700

    Create Password with symbols

---
+++ src/ts/Symbols.ts
@@ -0,0 +1,15 @@
+import { Settings } from "../../Settings.json";
+(() => {
+    let length = parseInt(Settings.length, 10);
+    let chars = Settings.chars.sym
+    let quantity = parseInt(Settings.quantity, 10);
+
+    for (let i = 1; i <= quantity; i++) {
+        let Password = "";
+        for (let j = 1; j <= length; j++) {
+            let randomnum = Math.floor(Math.random() * chars.length);
+            Password = Password.concat(chars[randomnum]);
+        }
+        console.log(`Password: ${Password}`);
+    }
+})();
