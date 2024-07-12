5ece377f7af6e3e564250e18edfcc40c9c660ae2
Tristan Simon <simontristan7766@gmail.com>
2024-07-12 07:43:04 +0200

    removed some <> anf : Elements

--- src/Password/ts/easy/Uppercase.ts
+++ src/Password/ts/easy/Uppercase.ts
@@ -1,18 +1,18 @@
 import { Settings } from "../../../../Settings.json";
 
-let length: number = <number>parseInt(Settings.PasswordSettings.length, 10);
-let chars: string = <string>Settings.PasswordSettings.chars.upper;
-let quantity: number = <number>parseInt(Settings.PasswordSettings.quantity, 10);
+let length= parseInt(Settings.PasswordSettings.length, 10);
+let chars= Settings.PasswordSettings.chars.upper;
+let quantity = parseInt(Settings.PasswordSettings.quantity, 10);
 
 let CreatePassword = (chars: string, length: number) => {
-    let password = <string>"";
-    for (let i: number = <number>1; i <= length; i++) {
-        let randomnum = <number>Math.floor(Math.random() * chars.length);
-        password = <string>password.concat(chars[randomnum]);
+    let password = "";
+    for (let i: number = 1; i <= length; i++) {
+        let randomnum = Math.floor(Math.random() * chars.length);
+        password = password.concat(chars[randomnum]);
     }
     console.log(`Password: ${password}`);
 };
 
-for (let i: number = <number>1; i <= quantity; i++) {
+for (let i = 1; i <= quantity; i++) {
     CreatePassword(chars, length);
 }
