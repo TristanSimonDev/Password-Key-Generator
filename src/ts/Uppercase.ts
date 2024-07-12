9f61be59b653093cf553b55e60e9959f03f5fb20
Tristan Simon <simontristan7766@gmail.com>
2024-07-12 02:22:17 +0200

    Made better functions in Uppercase

--- src/ts/Uppercase.ts
+++ src/ts/Uppercase.ts
@@ -1,15 +1,18 @@
 import { Settings } from "../../Settings.json";
-(() => {
-    let length = parseInt(Settings.length, 10);
-    let chars = Settings.chars.upper
-    let quantity = parseInt(Settings.quantity, 10);
 
-    for (let i = 1; i <= quantity; i++) {
-        let Password = "";
-        for (let j = 1; j <= length; j++) {
-            let randomnum = Math.floor(Math.random() * chars.length);
-            Password = Password.concat(chars[randomnum]);
-        }
-        console.log(`Password: ${Password}`);
+let length: number = <number>parseInt(Settings.PasswordSettings.length, 10);
+let chars: string = <string>Settings.PasswordSettings.chars.upper;
+let quantity: number = <number>parseInt(Settings.PasswordSettings.quantity, 10);
+
+let CreatePassword = (chars: string, length: number) => {
+    let password = <string>"";
+    for (let i: number = <number>1; i <= length; i++) {
+        let randomnum = <number>Math.floor(Math.random() * chars.length);
+        password = <string>password.concat(chars[randomnum]);
     }
-})();
+    console.log(`Password: ${password}`);
+};
+
+for (let i: number = <number>1; i <= quantity; i++) {
+    CreatePassword(chars, length);
+}
