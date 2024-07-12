dfc6d5a00b40a995ffdd768b07460125a8db0f16
Tristan Simon <simontristan7766@gmail.com>
2024-07-12 02:08:10 +0200

    Made better functions in Symbols

--- src/ts/Symbols.ts
+++ src/ts/Symbols.ts
@@ -1,10 +1,10 @@
 import { Settings } from "../../Settings.json";
 
-let length : number = <number>parseInt(Settings.PasswordSettings.length, 10);
-let chars : string = <string>Settings.PasswordSettings.chars.sym;
-let quantity : number = <number>parseInt(Settings.PasswordSettings.quantity, 10);
+let length: number = <number>parseInt(Settings.PasswordSettings.length, 10);
+let chars: string = <string>Settings.PasswordSettings.chars.sym;
+let quantity: number = <number>parseInt(Settings.PasswordSettings.quantity, 10);
 
-let CreatePassword = (chars : string, length: number) => {
+let CreatePassword = (chars: string, length: number) => {
     let password = <string>"";
     for (let i: number = <number>1; i <= length; i++) {
         let randomnum = <number>Math.floor(Math.random() * chars.length);
