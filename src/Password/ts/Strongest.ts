099dbe340ff2d93025bda2126b6445d3ffc9219d
Tristan Simon <simontristan7766@gmail.com>
2024-07-13 11:14:36 +0100

    add arrow function

--- src/Password/ts/Strongest.ts
+++ src/Password/ts/Strongest.ts
@@ -6,3 +6,7 @@ let CharSettings = Settings.PasswordSettings.chars
 let AllChars = Object.values(CharSettings).join('');
 const quantity = parseInt(Settings.PasswordSettings.quantity, 10)
 const length = parseInt(Settings.PasswordSettings.length)
+
+let CreatePassword = (AllChars: string, length: number) => {
+    
+}
