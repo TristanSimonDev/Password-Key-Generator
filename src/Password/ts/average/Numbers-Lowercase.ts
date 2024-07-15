f000b59b22bcf4b3a29832f9a75bf1079e569c5f
Tristan Simon <simontristan7766@gmail.com>
2024-07-15 13:59:53 +0100

    show average Numbers-Lowercase on creation

---
+++ src/Password/ts/average/Numbers-Lowercase.ts
@@ -0,0 +1,18 @@
+import { Settings } from '../../../../.vscode/Settings.json' //root json object
+
+let length= parseInt(Settings.PasswordSettings.length, 10);
+let chars= Settings.PasswordSettings.chars.num + Settings.PasswordSettings.chars.lower;
+let quantity = parseInt(Settings.PasswordSettings.quantity, 10);
+
+let CreatePassword = (chars: string, length: number) => {
+    let password = "";
+    for (let i: number = 1; i <= length; i++) {
+        let randomnum = Math.floor(Math.random() * chars.length);
+        password = password.concat(chars[randomnum]);
+    }
+    console.log(`Average Numbers-Lowercase: ${password}`);
+};
+
+for (let i = 1; i <= quantity; i++) {
+    CreatePassword(chars, length);
+}
