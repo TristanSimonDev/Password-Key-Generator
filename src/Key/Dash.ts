7389133afeeae11b21227c1dea54c8c9d1b5a77a
Tristan Simon <simontristan7766@gmail.com>
2024-07-15 12:31:33 +0100

    add quantity

--- src/Key/Dash.ts
+++ src/Key/Dash.ts
@@ -1,8 +1,10 @@
 import { Settings } from "../../.vscode/Settings.json";
 
+let Dash = Settings.KeySettings.TrimmSymbols.dash
 let Segments = parseInt(Settings.KeySettings.Segments);
 let chatsbetweentrimm = parseInt(Settings.KeySettings.Charsbetweentrimm)
 let chars = Settings.KeySettings.chars.upper + parseInt(Settings.KeySettings.chars.num, 10);
+let quantity = parseInt(Settings.KeySettings.quantity)
 
 
 let CreateKey = (chars: string, Segments: number) => {
@@ -10,13 +12,13 @@ let CreateKey = (chars: string, Segments: number) => {
     for (let i = 1; i <= Segments; i++) {   
         for (let j = 1; j <= chatsbetweentrimm + 1; j++) {
             let randomnum = Math.floor(Math.random() * chars.length);
-            j <= chatsbetweentrimm ? key = key.concat(chars[randomnum]) : key = key.concat("-")
+            j <= chatsbetweentrimm ? key = key.concat(chars[randomnum]) : key = key.concat(Dash)
         }          
     }
     key.endsWith("-") ? key = key.slice(0, -1) : null;
     console.log(`Key: ${key}`)   
 };
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= quantity; i++) {
     CreateKey(chars, Segments)
 }
