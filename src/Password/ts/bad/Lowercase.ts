bf2384097cc7dd81b19bb2f7610bef20f70f7578
Tristan Simon <simontristan7766@gmail.com>
2024-07-18 15:19:49 +0100

    import the function and use it

--- src/Password/ts/bad/Lowercase.ts
+++ src/Password/ts/bad/Lowercase.ts
@@ -1,18 +1,9 @@
 import { Settings } from '../../../../.vscode/Settings.json' //root json object
+import * as Function from '../../../function/Calc'
 
 let length= parseInt(Settings.PasswordSettings.length, 10);
 let chars= Settings.PasswordSettings.chars.lower;
 let quantity = parseInt(Settings.PasswordSettings.quantity, 10);
 
-let CreatePassword = (chars: string, length: number) => {
-    let password = "";
-    for (let i: number = 1; i <= length; i++) {
-        let randomnum = Math.floor(Math.random() * chars.length);
-        password = password.concat(chars[randomnum]);
-    }
-    console.log(`Bad Lowercase: ${password}`);
-};
 
-for (let i = 1; i <= quantity; i++) {
-    CreatePassword(chars, length);
-}
+Function.CreatePassword(chars, length, quantity, "test");
